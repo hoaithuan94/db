8ffc6576a82a4d1e4822ba2ebd515d6161ddb591
hoaithuan94 <hoaithuan.tran94@gmail.com>
2024-12-19 21:09:52 +0700

    Update answer.js

--- js/answer.js
+++ js/answer.js
@@ -1,4 +1,4 @@
-import { db } from "./init.js";
+import { db } from "js/init.js";
 import {collection, doc, addDoc, getDocs, setDoc, deleteDoc, serverTimestamp, onSnapshot, query } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-firestore.js";
 
 
